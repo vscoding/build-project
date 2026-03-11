4ef2acd65dff6abf0cbdc4785606a4ad54cbbfd4
zhenjie zhu <tech@intellij.io>
2026-03-11 17:54:08 +0800

    Update install_intellij.sh: replace deprecated IntelliJ mirror URL with updated iproute mirror URL in Ubuntu and Debian scripts

--- docker/install/ubuntu/install_intellij.sh
+++ docker/install/ubuntu/install_intellij.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # shellcheck disable=SC1090 disable=SC2086
 [ -z $ROOT_URI ] && source <(curl -sSL https://dev.kubectl.org/init)
-# ROOT_URI=https://dev.kubectl.net
 
 bash <(curl -sSL $ROOT_URI/docker/install/install_apt_tpl.sh) \
   "ubuntu" \
-  "https://mirrors.intellij.tech/container/docker-ce"
+  "https://mirrors.iproute.org/container/docker-ce"
