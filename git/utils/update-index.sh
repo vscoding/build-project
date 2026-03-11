79af0cf4f35ccc5b11d0949ba9cbd59cdd22a085
zhenjie zhu <tech@intellij.io>
2026-03-12 01:30:21 +0800

    Add update-index.sh: script to make .sh and shebang scripts executable using git update-index

---
+++ git/utils/update-index.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# shellcheck disable=SC2164,SC1090,SC2086
+[ -z $ROOT_URI ] && source <(curl -sSL https://dev.kubectl.org/init)
+echo -e "\033[0;32mROOT_URI=$ROOT_URI\033[0m"
+# export ROOT_URI=https://dev.kubectl.net
+source <(curl -sSL $ROOT_URI/func/log.sh)
+source <(curl -sSL $ROOT_URI/func/ostype.sh)
+
+if is_windows; then
+  log_info "build" "build in windows"
+  export MSYS_NO_PATHCONV=1
+fi
+
+set -euo pipefail
+
+log_info "chmod" "start make executable scan"
+
+curr_dir=$(pwd)
+log_info "chmod" "current directory: $curr_dir"
+read -p "chmod" "This script will scan all tracked files in git and make .sh files and shebang scripts executable. Do you want to continue? (y/n) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  log_info "chmod" "aborted by user"
+  exit 0
+fi
+
+# 1) Traditional .sh files (tracked by git)
+sh_changed=0
+while IFS= read -r f; do
+  git update-index --chmod=+x -- "$f"
+  sh_changed=$((sh_changed + 1))
+  log_info "chmod" "git update-index --chmod=+x $f (.sh)"
+done < <(git ls-files '*.sh')
+
+# 2) Suffix-less scripts with shebang (for example install_packages/run-script)
+# Only apply to tracked non-.sh files.
+shebang_changed=0
+while IFS= read -r -d '' f; do
+  [[ "$f" == *.sh ]] && continue
+  if head -c 2 "$f" | grep -q '^#!'; then
+    git update-index --chmod=+x -- "$f"
+    shebang_changed=$((shebang_changed + 1))
+    log_info "chmod" "git update-index --chmod=+x $f (shebang)"
+  fi
+done < <(git ls-files -z)
+
+log_info "chmod" "done, .sh changed: $sh_changed, shebang changed: $shebang_changed"
